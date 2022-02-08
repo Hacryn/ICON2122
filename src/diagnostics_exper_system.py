46a9e4dfa6ace12e4439ac610ee362ee5ed08adc
Enrico Ciciriello <21175992+Hacryn@users.noreply.github.com>
2022-02-08 15:27:19 +0100

    initial commit

---
+++ src/diagnostics_exper_system.py
@@ -0,0 +1,30 @@
+from experta import *
+
+def runex():
+    engine = DiagnosticsES()
+    engine.reset()
+    engine.run()
+
+def askquestion(question: str) -> bool:
+    answer = input(question + " (y/n): ")
+    while not isanswer(answer):
+        answer = input(question + " (y/n): ")
+
+    answer = answer.lower()
+    return answer == "y"
+
+def isanswer(answer: str) -> bool:
+    answer = answer.lower()
+    if answer == "y" or answer == "n": return True
+    else: return False
+
+class DiagnosticsES(KnowledgeEngine):
+
+    @DefFacts()
+    def _initial_action(self):
+        yield Fact(question = 1)
+
+    @Rule(Fact(question = 1))
+    def ask_nausea(self):
+        if askquestion("Hai la nausea?"): self.declare(Fact(nausea = True))
+        self.declare(Fact(question = 2))
---
+++ src/main.py
@@ -0,0 +1,11 @@
+from diagnostics_exper_system import runex
+
+def expert_system():
+    runex()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    expert_system()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
